4a9e1aaa9ace6ab618977bdfc49519454dcac7f5
ZhenKyle <zhenkyle@gmail.com>
2016-07-18 10:15:16 +0000

    second COMMIT

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+	var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+	var streams = [];
+	function getUserInfo(user) {};
+	function getStreamInfo(user){};
+    function processOneUser(element, index, array) {
+
+    }
+
+    function render() {
+    }
+
+    $(".btn-group .btn").click(function() {
+    	render();
+    });
+
+    $("#nameFilter").on('input',function() {
+    	console.log($(this).val());
+    });
+
+})
+
